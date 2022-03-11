d6880a6a5275d23bad4087f1d8147be2c6bafa4e
Nox Yu <lyu75@Noxs-MacBook-Pro.local>
2022-03-11 03:35:06 -0500

    started working on findin the limblenth problem

---
+++ evo_trees/limbLen/main.go
@@ -0,0 +1,8 @@
+/*
+Code Challenge: Solve the Limb Length Problem.
+
+Input: An integer n, followed by an integer j between 0 and n - 1, followed by a space-separated additive distance matrix D (whose elements are integers).
+Output: The limb length of the leaf in Tree(D) corresponding to row j of this distance matrix (use 0-based indexing).
+*/
+
+package main
