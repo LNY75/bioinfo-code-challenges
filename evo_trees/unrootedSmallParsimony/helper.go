0ab5692326f3ed696669dde5b641d4dd1ccd226b
Nox Yu <lyu75@Noxs-MacBook-Pro.local>
2022-03-27 00:16:44 -0400

    finished solving the smallParsimony problem for unrooted binary trees

---
+++ evo_trees/unrootedSmallParsimony/helper.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func PrintNodes(T []*Node) {
+	for _, node := range T {
+		if node != nil {
+			fmt.Print(node.id, " ", node.seq, " ")
+			if node.child1 != nil {
+				fmt.Print("node: ", node.id, " child1 id: ", node.child1.id, " child2 id: ", node.child2.id)
+			}
+			if node.parent != nil {
+				fmt.Print(" parent: ", node.parent.id, " len to parent: ", node.lenToParent)
+			}
+			fmt.Println(" ")
+		}
+	}
+}
+
+func PrintLeavesS(n int, s [][]int) {
+	for i := 0; i < n; i++ {
+		fmt.Println(s[i])
+	}
+}
+
+func PrintInternalNodeS(n int, s [][]int) {
+	for i := n; i < len(s); i++ {
+		fmt.Println(s[i])
+	}
+}
+
+func PrintAdj(T []*Node) {
+	for i := 0; i < len(T)-1; i++ {
+		node := T[i]
+		if node.parent != nil {
+			fmt.Printf("%s->%s:%d\n", node.seq, node.parent.seq, node.lenToParent)
+			fmt.Printf("%s->%s:%d\n", node.parent.seq, node.seq, node.lenToParent)
+		}
+	}
+}
